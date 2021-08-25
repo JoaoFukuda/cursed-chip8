8837a6b7b59294481b0ed3dbfcaf23d6ba9360a9
João Fukuda <joao.fukuda@protonmail.com>
2021-08-25 08:23:20 -0300

    Now both command and edit mode works as expected

--- src/chip.cpp
+++ src/chip.cpp
@@ -1,5 +1,53 @@
 #include "chip.hpp"
 
+#include <stdexcept>
+
+inline unsigned char char_to_hex(char c)
+{
+	switch (c) {
+		case '0':
+			return 0x0;
+		case '1':
+			return 0x1;
+		case '2':
+			return 0x2;
+		case '3':
+			return 0x3;
+		case '4':
+			return 0x4;
+		case '5':
+			return 0x5;
+		case '6':
+			return 0x6;
+		case '7':
+			return 0x7;
+		case '8':
+			return 0x8;
+		case '9':
+			return 0x9;
+		case 'a':
+		case 'A':
+			return 0xA;
+		case 'b':
+		case 'B':
+			return 0xB;
+		case 'c':
+		case 'C':
+			return 0xC;
+		case 'd':
+		case 'D':
+			return 0xD;
+		case 'e':
+		case 'E':
+			return 0xE;
+		case 'f':
+		case 'F':
+			return 0xF;
+		default:
+			throw std::runtime_error("Input not defined");
+	}
+}
+
 void cc8::Chip::draw_info(bool draw_value)
 {
 	for (unsigned char y = 23; y < 32; ++y) {
@@ -23,7 +71,8 @@ void cc8::Chip::command()
 {
 	draw_info();
 	m_memory.get_display().show();
-	switch (std::getchar()) {
+	char input = std::getchar();
+	switch (input) {
 		case 'o':
 			switch (std::getchar()) {
 				case '0':
@@ -48,6 +97,14 @@ void cc8::Chip::command()
 		case 'q':
 			m_mode = Mode::Quit;
 			break;
+		default:
+			m_address = (char_to_hex(input) << 12) + (m_address & 0x0FFF);
+			for (unsigned char i = 1; i < 4; ++i) {
+				draw_info();
+				m_memory.get_display().show();
+				m_address = (char_to_hex(std::getchar()) << (12 - i*4)) + (m_address & ~(0xF << (12 - i*4)));
+			}
+			break;
 	}
 }
 
@@ -55,12 +112,20 @@ void cc8::Chip::edit()
 {
 	draw_info(true);
 	m_memory.get_display().show();
-	switch (std::getchar()) {
+	char input = std::getchar();
+	switch (input) {
 		case 'o':
 			++m_address;
 			break;
 		case 't':
 			m_mode = Mode::Command;
+			break;
+		default:
+			m_memory[m_address] = (char_to_hex(input) << 4) + (m_memory[m_address] & 0x0F);
+			draw_info(true);
+			m_memory.get_display().show();
+			m_memory[m_address] = char_to_hex(std::getchar()) + (m_memory[m_address] & 0xF0);
+			++m_address;
 			break;
 	}
 }
